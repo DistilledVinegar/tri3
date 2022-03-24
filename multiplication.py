d34274dd0dcba2c8b5be055142c671d7108e9bdf
DistilledVinegar <66929135+DistilledVinegar@users.noreply.github.com>
2022-03-23 22:41:35 -0700

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,13 +1,13 @@
 #multiplication table of any two difit number
 
-num = input("What number do you want to use? ")
+nums = [87, 38, 76]
 
-strnum = str(num)
-
-digit1 = int(strnum[0])
-digit2 = int(strnum[1]) #seperate the numbers
-
-for i in range(10):
-    digit1 += int(strnum[0])
-    digit2 += int(strnum[1])
-    print(str(digit1) + "   " + str(digit2)[0] + " " + str(digit2)[1] + "   " + "(" + str(digit1) + "+" + str(digit2)[0] + ")    " + str(digit1+int(str(digit2)[0])) + str(digit2)[1])
+for i in nums:
+    print("Now doing: " + str(i))
+    strnum = str(i)
+    digit1 = int(strnum[0])
+    digit2 = int(strnum[1]) #seperate the numbers
+    for j in range(10):
+        digit1 += int(strnum[0])
+        digit2 += int(strnum[1])
+        print(str(digit1) + "   " + str(digit2)[0] + " " + str(digit2)[1] + "   " + "(" + str(digit1) + "+" + str(digit2)[0] + ")    " + str(digit1+int(str(digit2)[0])) + str(digit2)[1])
