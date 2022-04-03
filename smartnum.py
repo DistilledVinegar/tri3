069c5fd1fc76f71ca8baa0122b1c2788de354e92
DistilledVinegar <66929135+DistilledVinegar@users.noreply.github.com>
2022-04-02 20:57:26 -0700

    it works i guess
    hi ms naidu

--- smartnum.py
+++ smartnum.py
@@ -1,5 +1,4 @@
-#Smart Number Guesser
-
+#rewrite of numguesser
 import math, random
 
 class Number:
@@ -20,86 +19,56 @@ def isPrime(x):
         return False
     return True
 
-bung = []
-
-for i in range(100):
-
-    digits = len(str(i))
-    
-    primeness = isPrime(i)
-    
-    if math.sqrt(i) % 10 == 0: #check if the number cleanly squares
-        square = True #math library my beloved
-    else:
-        square = False
-        
-    if i % 2 == 0:
-        oddness = False
-    else:
-        oddness = True
-
-    if reversed(str(i)) == str(i):
-        mirrored = True
-    else:
-        mirrored = False
-
-    value = i
-
-    glutton = Number(digits, primeness, square, oddness, mirrored, value)
-
-    bung.append(glutton)
-
+questionList = ["Is your number prime? ", "Is your number square? ", "Is your number odd? ", "Is your number mirrored? "]
 def questionAsker(question):
     answer = input(question)
     while answer not in ["yes", "no", "Yes", "No"]:
         print("no no no this is ALL wrong")
         answer = input(question)
-    return answer
-        
-questionList = ["Is your number prime? ", "Is your number square? ", "Is your number odd? ", "Is your number mirrored? "]
+    if answer in ["yes", "Yes"]:
+        return True
+    else:
+        return False
+
+bung = [] #two separate dictionaries have to be used or else it will skip number after removing them causing it to break :/
+sniff = []
+
+for i in range(10000): #this is very important i think
+    bung.append(Number(len(str(i)), isPrime(i), eval("math.sqrt(i)*10 % 10 == 0"), eval("i % 2 == 1"), eval("str(i)[::-1] == str(i)"), i))
 
 digits = int(input("How many digits is your number? "))
-for num in bung: 
-    if num.digits != digits:
-        print("removing " + str(num.value))
-        bung.remove(num)
-        print(len(bung))
-        
+for i in bung:
+    if i.digits == digits:
+        sniff.append(i)
+
+bung = sniff
+sniff = []
 primeness = questionAsker(questionList[0])
-for num in bung: 
-    if num.primeness != primeness:
-        print("removing " + str(num.value))
-        bung.remove(num)
+for i in bung:
+    if i.primeness == primeness:
+        sniff.append(i)
 
-squareness = questionAsker(questionList[1])
-for num in bung: 
-    if num.square != squareness:
-        print("removing " + str(num.value))
-        bung.remove(num)
-        
+bung = sniff
+sniff = []
+square = questionAsker(questionList[1])
+for i in bung:
+    if i.square == square:
+        sniff.append(i)
+
+bung = sniff
+sniff = []
 oddness = questionAsker(questionList[2])
-for num in bung: 
-    if num.oddness != oddness:
-        print("removing " + str(num.value))
-        bung.remove(num)
-        
-mirrored = questionAsker(questionList[3])
-for num in bung: 
-    if num.mirrored != mirrored:
-        print("removing " + str(num.value))
-        bung.remove(num)
+for i in bung:
+    if i.oddness == oddness:
+        sniff.append(i)
 
-"""for num in bung: #Hackathon challenge: Rewrite this portion using a dictionary
-    if num.digits != digits:
-        bung.remove(num)
-    elif num.primeness != primeness:
-        bung.remove(num)
-    elif num.square != squareness:
-        bung.remove(num)
-    elif num.oddness != oddness:
-        bung.remove(num)
-    elif num.mirrored != mirrored:
-        bung.remove(num)"""
+bung = sniff
+sniff = []
+mirrored = questionAsker(questionList[3])
+for i in bung:
+    if i.mirrored == mirrored:
+        sniff.append(i)
+
 
 for i in bung:
-    print(i.value)
+    print(str(i.value))
